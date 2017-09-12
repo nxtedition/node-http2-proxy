c6cff9c36f3c764ac086a6fb5190cd1d077bfe6a
Robert Nagy <ronagy@icloud.com>
2017-09-12 21:52:24 +0200

    fix: bench

--- benchmark/proxy.js
+++ benchmark/proxy.js
@@ -1,10 +1,7 @@
 const proxy = require('../')
-require('http2').createServer({
-  allowHTTP1: true,
-  onRequest: function (req, res) {
-    proxy.web(req, res, {
-      hostname: 'localhost',
-      port: 9000
-    })
-  }
+require('http').createServer(function (req, res) {
+  proxy.web(req, res, {
+    hostname: 'localhost',
+    port: 9000
+  }, err => console.error(err))
 }).listen(8000)
