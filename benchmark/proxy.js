508bb7f9dfb21c8cbb525ed70ce7421e6fb2a804
Robert Nagy <ronagy@icloud.com>
2017-11-05 17:14:42 +0100

    fix(benchmark): don't log undefined

--- benchmark/proxy.js
+++ benchmark/proxy.js
@@ -3,5 +3,5 @@ require('http').createServer(function (req, res) {
   proxy.web(req, res, {
     hostname: 'localhost',
     port: 9000
-  }, err => console.error(err))
+  }, err => err && console.error(err))
 }).listen(8000)
